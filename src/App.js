b3fb30d88795656b0c1006f55ed2ef0fc6a784ff
Chimoha31 <mokochii1108@gmail.com>
2022-04-15 13:52:10 -0700

    Created cimponents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Account from './components/Account';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
 const App = () => {
   return (
     <div>
-      
+      <h1 className="text-center text-3xl font-bold">
+        Firebase Authentication & Context
+      </h1>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
     </div>
   )
 }
