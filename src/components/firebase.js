78aa5fb32fe4a60866388aae0d92d116f17b9441
Chimoha31 <mokochii1108@gmail.com>
2022-04-15 22:40:05 -0700

    pass current user

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -7,10 +7,10 @@ const firebaseConfig = {
   projectId: "fir-auth-d1920",
   storageBucket: "fir-auth-d1920.appspot.com",
   messagingSenderId: "559272794482",
-  appId: "1:559272794482:web:469c0db0a5531b9b9fe5eb"
+  appId: "1:559272794482:web:469c0db0a5531b9b9fe5eb",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-export default app
+export default app;
